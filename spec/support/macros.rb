5a6c0fae6c3272d678a68305d43932193d041cf2
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-06-11 21:53:36 +0200

    listing users for each account

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -25,8 +25,16 @@ def sign_up(subdomain)
 end
 
 def sign_user_in(user, opts={})
-  visit new_user_session_url(subdomain: opts[:subdomain])
+  if opts[:subdomain]
+    visit new_user_session_url(subdomain: opts[:subdomain])
+  else
+    visit new_user_session_path
+  end
   fill_in 'Email', with: user.email
   fill_in 'Password', with: (opts[:password] || user.password)
   click_button 'Sign in'
+end
+
+def set_subdomain(subdomain)
+  Capybara.app_host = "http://#{subdomain}.example.com"
 end
